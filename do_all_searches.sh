706d81bf6f60f0f435a55efabfeed4c4756e1833
Frank Lam <frank@almostflan.com>
2012-07-14 15:02:40 -0700

    init commit - yelp api querying

---
+++ do_all_searches.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python search.py --consumer_key="KthO8dNdeYTgBFnBlIIdHA" --consumer_secret="Tm8COLLLfk2U5_YQmBEpSZtN6p4" --token="wQqdIJKCWDFNTSunGYjsoC-ebF3qy7lo" --token_secret="IXMeyVMwlKawX9yNJxCBXS-Zo4Y" --location="seattle" --term="hipster bars"
+
