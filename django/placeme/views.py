deee55aad55f21a17ccb7d8354aa932de8743556
Frank Lam <frank@almostflan.com>
2012-07-14 21:39:47 -0700

    more jsons, enabled writing

--- django/placeme/views.py
+++ django/placeme/views.py
@@ -58,15 +58,14 @@ def query(request):
     )
 
     json_cache = os.path.join(os.getcwd(), 'json_cache', cache_name)
-    print 'finding {0}'.format(json_cache)
     json_resp = ''
     if (os.path.exists(json_cache)):
         json_resp = open(json_cache, 'r').read()
-    #else:
-        #json_resp = yelp_request({'term': term, 'location': location})
-        #out_cache = open(json_cache, 'w')
-        #out_cache.write(json_resp)
-    #    out_cache.close()
+    else:
+        json_resp = yelp_request({'term': term, 'location': location})
+        out_cache = open(json_cache, 'w')
+        out_cache.write(json_resp)
+        out_cache.close()
 
     return HttpResponse(json_resp, mimetype='application/json')
 
